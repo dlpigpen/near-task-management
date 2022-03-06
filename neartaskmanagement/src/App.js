d250044f78f10a33f9979dd10b7577101a306b6f
Duc Nguyen <emngok123@gmail.com>
2022-03-06 12:30:22 +0700

    hide form add

--- neartaskmanagement/src/App.js
+++ neartaskmanagement/src/App.js
@@ -13,6 +13,8 @@ const { networkId } = getConfig(process.env.NODE_ENV || 'development')
 
 export default function App() {
 
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -50,9 +52,9 @@ export default function App() {
 
   // add task
   const addTask = (task) => {
-      const id = Math.floor(Math.random() * 10000) + 1
-      const newTask = { id , ...task }
-      setTasks([...tasks, newTask])
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
   }
 
 
@@ -110,8 +112,12 @@ export default function App() {
       </button>
 
       <div className="container">
-        <Header />
-        <AddTask onAdd={addTask} />
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+
+        {showAddTask && <AddTask onAdd={addTask} />}
         {tasks.length > 0 ?
           (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) :
           ('No task to show')
--- neartaskmanagement/src/components/Header.js
+++ neartaskmanagement/src/components/Header.js
@@ -2,24 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './button'
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log("e");
-    }
-    
+const Header = ({ title, onAdd, showAdd }) => {
+  console.log(onAdd)
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      <Button
+        color={showAdd ? 'red' : 'green'}
+        text={showAdd ? 'Close' : 'Add'}
+        onClick={onAdd}
+      />
     </header>
   )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker'
+  title: 'Task Tracker'
 }
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 }
 export default Header
