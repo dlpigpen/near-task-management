d92007f53e1987535b8341aa4c80ebd3d33b2694
Duc Nguyen <emngok123@gmail.com>
2022-03-06 22:57:15 +0700

    update task tracker contract

--- neartaskmanagement/contract/src/lib.rs
+++ neartaskmanagement/contract/src/lib.rs
@@ -13,12 +13,8 @@
 
 // To conserve gas, efficient serialization is achieved through Borsh (http://borsh.io/)
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LookupMap, UnorderedMap};
+use near_sdk::collections::{LookupMap};
 use near_sdk::{env, near_bindgen, setup_alloc};
-extern crate uuid;
-use uuid::Uuid;
-
-pub mod account;
 
 setup_alloc!();
 
@@ -74,12 +70,12 @@ impl Tasks {
         let account_id = env::signer_account_id();
         let contains_user = self.tasks.contains_key(&account_id);
 
-        let udid = Uuid::new_v4().to_hyphenated_string();
+        let random_id = self.generate_id();
         let task = Task::new( 
-            udid,
+            random_id,
             text,
             day,
-             reminder
+            reminder
         );
 
         if contains_user {
@@ -97,11 +93,115 @@ impl Tasks {
         }
     }
 
-    pub fn get_tasks(self, user: String) -> Vec<Task> {
-        let tasks = match self.tasks.get(&user) {
+    pub fn get_tasks(&self) -> Vec<Task> {
+        let account_id = env::signer_account_id();
+        let tasks = match self.tasks.get(&account_id) {
             Some(x) => x,
             None => vec![],
         };
         return tasks;
     }
+
+    fn generate_id(&self) -> String {
+        let account_id = env::signer_account_id();
+        let total_task = self.get_total_task();
+        let next_id = (total_task + 1).to_string();
+        let new_id = [account_id, next_id].join("-");
+        return new_id;
+    }
+
+    pub fn delete_a_task(&mut self, task_id: String) -> bool {
+        let account_id = env::signer_account_id();
+        let mut user_tasks = self.get_tasks();
+        if user_tasks.len() > 0 {
+            let index = user_tasks.iter().position(|x| *x.id == task_id).unwrap();
+            user_tasks.remove(index);
+            self.tasks.insert(&account_id, &user_tasks);
+            return true
+        }
+        return false
+    }
+
+    pub fn get_total_task(&self) -> usize {
+        let tasks  = self.get_tasks();
+        return tasks.len();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
+
+    fn get_context(predecessor_account_id: String, storage_usage: u64) -> VMContext {
+        VMContext {
+            current_account_id: "alice.testnet".to_string(),
+            signer_account_id: "mitsori1.testnet".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id,
+            input: vec![],
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage,
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view: false,
+            output_data_receivers: vec![],
+            epoch_height: 19,
+        }
+    }
+
+    #[test]
+    fn get_current_total_task() {
+        let context = get_context(String::from("mitsori1.testnet"), 0);
+        testing_env!(context);
+        let mut contract = Tasks::default();
+        contract.add_task(
+            String::from("day1"),
+            String::from("date1"),
+            true
+        );
+        let total_task = contract.get_total_task();
+        println!("Total task after increment: {}", total_task);
+        assert_eq!(1, total_task, "Expected empty");
+    }
+
+    #[test]
+    fn get_task_id() {
+        let context = get_context(String::from("mitsori1.testnet"), 0);
+        testing_env!(context);
+        let contract = Tasks::default();
+        let unique_id = contract.generate_id();
+        println!("Unique id: {}", unique_id);
+        assert_eq!(unique_id, "mitsori1.testnet-1", "Does not match id");
+    }
+
+    #[test]
+    fn get_delete_task() {
+        let context = get_context(String::from("mitsori1.testnet"), 0);
+        testing_env!(context);
+        let mut contract = Tasks::default();
+         contract.add_task(
+            String::from("day1"),
+            String::from("date1"),
+            true
+        );
+        let total_after_added = contract.get_total_task();
+        assert_eq!(1, total_after_added, "Add function broken");
+
+        let tasks = contract.get_tasks();
+        let first_task = tasks.first().unwrap();
+        assert_eq!(first_task.id, "mitsori1.testnet-1", "id does not match");
+
+        let task_id = String::from(&first_task.id);
+        contract.delete_a_task(task_id);
+        let total_after_deleted = contract.get_total_task();
+        assert_eq!(0, total_after_deleted, "After deleted wrong number");
+
+    }
+
 }
