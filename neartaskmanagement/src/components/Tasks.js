dfe08e689cb57eae852a2317f8c267c008504f3c
Duc Nguyen <emngok123@gmail.com>
2022-03-06 11:59:40 +0700

    delete a task

---
+++ neartaskmanagement/src/components/Tasks.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Task from './Task'
+
+export default function Tasks({ tasks, onDelete }) {
+    return (
+        <>
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} onDelete={onDelete} />
+            ))}
+        </>
+    )
+}
+
